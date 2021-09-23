1dc6acac6c457a5008568fb1e4984b3f80279bc7
salmabutlam01 <s613552052@phuket.psu.ac.th>
2021-09-23 17:45:06 +0700

    Test

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import  React  from  'react'; 
+import  {  StyleSheet,  Text,  View  }  from  'react-native'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export  default  class  App  extends  React.Component  { 
+  doIt  =  ()  =>  { 
+    console.log("Hello  from  console") 
+  } 
+  render()  { 
+    return  ( 
+     <View  style={styles.container}> 
+      <Text  onPress={this.doIt}>Hello  World</Text>            
+    </View> 
+    ); 
+  } 
+} 
+const  styles  =  StyleSheet.create({ 
+  container:  { 
+    flex:  1, 
+    backgroundColor:  '#fff', 
+    alignItems:  'center', 
+    justifyContent:  'center', 
+  }, 
+}); 
